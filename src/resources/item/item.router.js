753182880820387c34f18eb25bdd1529a2f4d820
Jean <jsoracipa@unal.edu.co>
2021-01-29 21:29:45 -0500

    Routes, error

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,18 @@
 import { Router } from 'express'
 
 const router = Router()
-
+const controller = (req,res) => {
+    res.send({ message: 'hello'})
+}
+// /api/item
+router
+    .route('/')
+    .get(controller)
+    .post(controller)
+// /api/item/:id
+router
+    .route('/:id')
+    .put(controller)
+    .delete(controller)
+    .get(controller)
 export default router
